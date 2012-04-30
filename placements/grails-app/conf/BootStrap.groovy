f61cc98e4aec00840d55caacaff3a193b6c8c02b
Jordan Sutcliffe <jordansutcliffe@hotmail.co.uk>
2012-04-30 17:51:43 +0100

    Used bootstrap for status codes

--- placements/grails-app/conf/BootStrap.groovy
+++ placements/grails-app/conf/BootStrap.groovy
@@ -1,7 +1,19 @@
+import placements.Status
+
 class BootStrap {
 
     def init = { servletContext ->
+
+  def applied = new Status(code:'001',description:'Applied').save();
+  def notinvited = new Status(code:'002',description:'Not invited for interview').save();
+def invited = new Status(code:'003',description:'Invited for interview').save();
+def notoffered = new Status(code:'004',description:'Not offered').save();
+def offered = new Status(code:'005',description:'Offered').save();
+def accepted = new Status(code:'006',description:'Accepted').save();
+def rejected = new Status(code:'007',description:'Rejected').save();
+
     }
     def destroy = {
     }
 }
+
